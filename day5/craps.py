016088e3a850da1ef80d20159f7f18383c365320
Marcus Liu <me@mail.lmarcus.onl>
2019-05-17 04:30:25 +0800

    總之先把day5完成

--- day5/craps.py
+++ day5/craps.py
@@ -5,3 +5,45 @@
 玩家有1000元的賭注，全部輸光則遊戲結束。
 
 """
+
+from random import randint
+
+asset = 1000
+
+while asset > 0:
+    print ('總注金：', asset)
+    gamble_continue = False
+    while True:
+        bet = int(input('請下注：'))
+        if bet > 0 and bet <= asset:
+            break
+    first_gamble = randint(1, 6) + randint(1, 6)
+    print('玩家搖出了%d點' % first_gamble)
+    if first_gamble == 7 or first_gamble == 11:
+        print('贏得彩金！')
+        asset += bet
+    elif first_gamble == 2 or first_gamble == 3 or first_gamble == 12:
+        print('輸去彩金！')
+        asset -= bet
+    else: 
+        gamble_continue = True
+     
+    while gamble_continue:
+        print('第二次對決！')
+        second_gamble = randint(1, 6) + randint(1, 6)
+        print('玩家搖出了%d點' % second_gamble)
+
+        if second_gamble == 7:
+            print('莊家勝！')
+            asset -= bet
+            gamble_continue = False
+        elif second_gamble == first_gamble:
+            print('玩家勝！')
+            asset += bet
+            gamble_continue = False
+
+
+print('沒錢啦！')
+
+   
+
